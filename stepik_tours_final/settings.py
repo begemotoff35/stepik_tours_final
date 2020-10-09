b30c8fc0e9dd68c6d44577284b67581425c78f53
begemotoff35 <begemotoff35@yandex.ru>
2020-10-09 17:57:02 +0300

    disabled debug_toolbar

--- stepik_tours_final/settings.py
+++ stepik_tours_final/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tours',
-    'debug_toolbar',
+    # 'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
+    # 'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'stepik_tours_final.urls'
@@ -128,6 +128,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # Для работы Debug Toolbar добавляем наш локальный IP
-INTERNAL_IPS = [
-    '127.0.0.1',
-]
+# INTERNAL_IPS = [
+#    '127.0.0.1',
+# ]
